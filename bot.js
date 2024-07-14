b0c03bbb3c907afc4feaf98106cfcc25dfc9282c
RAQKID <officialraqkid@gmail.com>
2024-07-14 13:36:56 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,10 +6,10 @@ const { Classic } = require("musicard");
 const { prefix } = require('./config.json');
 const nodes = [
      {
-    host: "37.114.42.191",
-    port: 7519, 
-    password: "danteisnttaken", 
-     secure: false
+    host: "lavalinkv4.serenetia.com",
+    port: 443, 
+    password: "lavalinkv4", 
+     secure: true
   },
 ];
 
